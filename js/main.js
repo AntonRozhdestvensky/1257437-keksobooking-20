f9f1406d7a32e63ebdb31c88f06dabab40fb50a9
Anton Rozhdestvensky <anton@rozhdestvensky.io>
2020-06-14 13:54:21 +0300

    Мелкие правки в форматировании кода

--- js/main.js
+++ js/main.js
@@ -2,14 +2,16 @@
 
 var ARRAY_ADS = 8;
 var AVATAR_TEMPLATE = 'img/avatars/user{{xx}}.png';
-var TITLE_ADS = ['Заголовок - 1',
+var TITLE_ADS = [
+             'Заголовок - 1',
              'Заголовок - 2',
              'Заголовок - 3',
              'Заголовок - 4',
              'Заголовок - 5',
              'Заголовок - 6',
              'Заголовок - 7',
-             'Заголовок - 8']; // Временно. Возможно будет изменен.
+             'Заголовок - 8'
+            ]; // Временно. Возможно будет изменен.
 var PRICE_MIN = 300;
 var PRICE_MAX = 100000;
 var TYPE_REALTY = ['palace', 'flat', 'house', 'bungalo'];
@@ -18,17 +20,21 @@ var GUEST = [1, 2, 3, 4]; // 3 === любое количество гостей,
 var CHECKIN = ['12:00', '13:00', '14:00'];
 var CHECKOUT = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var DESCRIPTION_ADS = ['Описание - 1',
+var DESCRIPTION_ADS = [
+                   'Описание - 1',
                    'Описание - 2',
                    'Описание - 3',
                    'Описание - 4',
                    'Описание - 5',
                    'Описание - 6',
                    'Описание - 7',
-                   'Описание - 8',]; // Временно. Возможно будет изменено.
-var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+                   'Описание - 8',
+                  ]; // Временно. Возможно будет изменено.
+var PHOTOS = [
+              'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
               'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-              'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+              'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+            ];
 var LOCATION_X_MIN = 0;
 var LOCATION_X_MAX = 1200;
 var LOCATION_Y_MIN = 130;
